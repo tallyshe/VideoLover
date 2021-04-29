a82c3813d1dfaa11820671002677974320b7a7f8
karhoshe <karhoshe@me.com>
2021-04-29 17:07:33 +0800

    Initial Commit

---
+++ VideoLover/VideoLoverApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoLoverApp.swift
+//  VideoLover
+//
+//  Created by Kar Ho She on 29/4/2021.
+//
+
+import SwiftUI
+
+@main
+struct VideoLoverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
