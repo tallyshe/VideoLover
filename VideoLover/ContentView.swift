4334c32c92d43faecf70527f0247b01479c05155
karhoshe <karhoshe@me.com>
2021-04-29 17:24:37 +0800

    Imported Video Model - Model Struct - Conform to Identifiable
    Content View
    - import the video array model
    - render the model list

--- VideoLover/ContentView.swift
+++ VideoLover/ContentView.swift
@@ -8,9 +8,14 @@
 import SwiftUI
 
 struct ContentView: View {
+    var videos: [Video] = VideoList.topTen
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        List(videos, id: \.id) { video in
+            Text(video.title)
+                .padding()
+        }
+
     }
 }
 
